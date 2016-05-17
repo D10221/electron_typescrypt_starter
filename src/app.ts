2ede98de83019d1c2de4ecbb8ec6685141424ffd
D10221 <danieliozzi@gmail.com>
2016-05-17 07:23:50 +0200

    .gitignore -> .idea

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
-(()=>{
+(()=> {
     var angular = require('angular');
 
     var ngRoute = require('angular-route');
-    
+
     console.log(ngRoute);
 
-class AppController {
-   
-   static $inject = ['$scope'];
-   
-   brand = 'tiny-x';
-   
-   constructor($scope){
-       
-   }
-}
+    class AppController {
 
-angular.module('app', [
-    'ngRoute'
-])
-.controller('AppController', AppController);
+        static $inject = ['$scope'];
 
-})()
+        brand = 'tiny-x';
+
+        constructor($scope) {
+
+        }
+    }
+
+    angular.module('app', [
+        'ngRoute'
+    ])
+        .controller('AppController', AppController);
+
+})();
